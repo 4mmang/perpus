4a20044ce1eee3e2ecc90d74bd3373452dbdd290
4mmang <ammangdeveloper@gmail.com>
2026-02-13 20:14:06 +0800

    tes

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -15,7 +15,7 @@
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
-            <a class="nav-link" href="">
+            <a class="nav-link" href="{{ url('/') }}">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
                 <span>Dashboard</span></a>
         </li>
