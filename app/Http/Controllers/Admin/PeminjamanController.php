d810ec2b338a35ff63c569c429f0f956cb9c8332
4mmang <ammangdeveloper@gmail.com>
2026-02-16 19:52:06 +0800

    tes

--- app/Http/Controllers/Admin/PeminjamanController.php
+++ app/Http/Controllers/Admin/PeminjamanController.php
@@ -17,11 +17,19 @@ class PeminjamanController extends Controller
     {
         $borrowing = \App\Models\BookLending::findOrFail($id);
         $borrowing->status = $request->input('status');
+        $book = \App\Models\Book::find($borrowing->book_id);
         if ($request->input('status') == 'borrowed') {
             $borrowing->lend_date = now();
         } elseif ($request->input('status') == 'returned') {
+            $book->stock += 1;
+            $borrowing->return_date = now();
+        } elseif ($request->input('status') == 'rejected') {
+            $book->stock += 1;
+        } elseif ($request->input('status') == 'overdue') {
+            $book->stock += 1;
             $borrowing->return_date = now();
         }
+        $book->save();
         $borrowing->save();
 
         return redirect()->route('peminjaman.index')->with('success', 'Status peminjaman berhasil diperbarui.');
